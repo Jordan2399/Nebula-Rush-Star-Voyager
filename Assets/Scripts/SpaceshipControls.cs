ffd33bc193ed974283fead6316566f7a49fce300
Cris9807 <72547498+Cris9807@users.noreply.github.com>
2024-02-19 22:54:33 +0100

    Input controls

--- Assets/Scripts/SpaceshipControls.cs
+++ Assets/Scripts/SpaceshipControls.cs
@@ -14,14 +14,7 @@ public class CubeControls : MonoBehaviour
 
     private Vector2 moveDirection;
     
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
         moveDirection = move.action.ReadValue<Vector2>();
     }
@@ -31,40 +24,9 @@ public class CubeControls : MonoBehaviour
         rigidbody.velocity = new Vector2(moveDirection.x * movingSpeed, moveDirection.y * movingSpeed);
     }
 
-    public void MoveUp()
+    public void Fire()
     {
-        Debug.Log("Up");
-        var position = cubeTransform.position;
-        float y = position.y;
-        y += movingSpeed/50f;
-        var newPosition = new Vector3(position.x, y, position.z);
-        cubeTransform.position = newPosition;
-    }
-
-    public void MoveDown()
-    {
-        var position = cubeTransform.position;
-        float y = position.y;
-        y -= movingSpeed/50f;
-        var newPosition = new Vector3(position.x, y, position.z);
-        cubeTransform.position = newPosition;
-    }
-
-    public void MoveLeft()
-    {
-        var position = cubeTransform.position;
-        float x = position.x;
-        x -= movingSpeed/50f;
-        var newPosition = new Vector3(x, position.y, position.z);
-        cubeTransform.position = newPosition;
-    }
-
-    public void MoveRight()
-    {
-        var position = cubeTransform.position;
-        float x = position.x;
-        x += movingSpeed/50f;
-        var newPosition = new Vector3(x, position.y, position.z);
-        cubeTransform.position = newPosition;
+        //TODO Fire bullets
+        Debug.Log("Fire");
     }
 }
